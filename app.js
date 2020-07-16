9a8320baa371191c817ac7ddfb83634b754c06c7
EC2 Default User <ec2-user@ip-172-31-7-0.us-west-1.compute.internal>
2020-07-16 17:43:54 +0000

    hw1 redo

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get("/intel", function(req, res){
 
 app.get("/sources", function(req, res){
     res.render("sources.ejs", {
-        contactCard: faker.helpers.createCard()
+        randomCard: faker.helpers.createCard()
     });
 });
 
